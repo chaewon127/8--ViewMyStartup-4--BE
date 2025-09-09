5f0d2050718a545f6a30e909d658e5a307752153
chaewon127 <osobi621@gmail.com>
2025-09-09 17:16:01 +0900

    feat: swagger 연결

--- src/routes/investmentRoute.js
+++ src/routes/investmentRoute.js
@@ -4,12 +4,51 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /investment/investments:
+ * get:
+ * summary: 투자 리스트 조회
+ * tags: [Investment]
+ * responses:
+ * 200:
+ * description: 투자 리스트
+ * content:
+ * application/json:
+ * schema:
+ * type: array
+ * items:
+ * $ref: '#/components/schemas/Investment'
+ */
 router.get("/investments", asyncHandler(investmentController.getInvestments));
 
+/**
+ * @openapi
+ * /investment/corp/{id}:
+ * get:
+ * summary: 특정 기업 투자 댓글 조회
+ * tags: [Investment]
+ * parameters:
+ * - in: path
+ * name: id
+ * required: true
+ * schema:
+ * type: string
+ * responses:
+ * 200:
+ * description: 댓글 리스트
+ * content:
+ * application/json:
+ * schema:
+ * type: array
+ * items:
+ * $ref: '#/components/schemas/Comment'
+ */
 router.get(
   "/corp/:id",
   asyncHandler(investmentController.getInvestmentComments)
 );
+
 router.post(
   "/corp/:id",
   asyncHandler(investmentController.createInvestmentComment)
