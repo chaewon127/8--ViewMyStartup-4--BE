3170f40c87859f39d04ea2a6b70fb457eb79bf01
chaewon127 <osobi621@gmail.com>
2025-09-10 13:52:46 +0900

    스웨거 주소 변경

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,3 +1,4 @@
+import { define } from "superstruct";
 import swaggerJSDoc from "swagger-jsdoc";
 
 const options = {
@@ -8,48 +9,9 @@ const options = {
       version: "1.0.0",
       description: "ViewMyStartup 백엔드 API 문서",
     },
-    servers: [{ url: "http://localhost:5555", description: "Local" }],
-    components: {
-      schemas: {
-        Investment: {
-          type: "object",
-          properties: {
-            id: { type: "string" },
-            name: { type: "string" },
-            virtual: { type: "number" },
-            createdAt: { type: "string", format: "date-time" },
-            updatedAt: { type: "string", format: "date-time" },
-          },
-        },
-        Comment: {
-          type: "object",
-          properties: {
-            id: { type: "string" },
-            content: { type: "string" },
-            createdAt: { type: "string", format: "date-time" },
-            updatedAt: { type: "string", format: "date-time" },
-          },
-        },
-        Corp: {
-          type: "object",
-          properties: {
-            id: { type: "string" },
-            name: { type: "string" },
-            industry: { type: "string" },
-          },
-        },
-        Compare: {
-          type: "object",
-          properties: {
-            id: { type: "string" },
-            corpId: { type: "string" },
-            selectedAt: { type: "string", format: "date-time" },
-          },
-        },
-      },
-    },
+    servers: [{ url: "http://localhost:3000", description: "Local" }],
   },
-  apis: ["./routes/*.js"],
+  apis: ["./src/routes/*.js"],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
